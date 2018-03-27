c69ab1b197a51cdac9f241ed22d1207cffe0b01c
walterfcarvalho@gmail.com <walterfcarvalho@gmail.com>
2018-03-27 11:39:08 -0300

    comit 3 do projeto

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -1,9 +1,9 @@
-var mysql  = require('mysql');
+var mysql = require('mysql');
 
-    console.log("variavel process.env.NODE_ENV", process.env.NODE_ENV);
+console.log("variavel process.env.NODE_ENV", process.env.NODE_ENV);
 
 function createDBConnection() {
-/*    if(process.env.NODE_ENV) {
+    /*    if(process.env.NODE_ENV) {
         return mysql.createConnection({
             host: 'localhost',
             user: 'root',
@@ -11,19 +11,20 @@ function createDBConnection() {
             database: 'cursonode'
         });
     }
-*/
-// mysql://b3a77af212021b:42310e7e@us-cdbr-iron-east-05.cleardb.net/heroku_87e9a1ece7b4c96?reconnect=true
-if (process.env.NODE_ENV == 'production') {
-    var url = process.env.CLEARDB_DATABASE_URL;
-    var grupos = url.match(/mysql:\/\/(.*):(.*)@(.*)\/(.*)\?/);
-    return mysql.createConnection({
-        host:grupos[3],
-        user:grupos[1],
-        password:grupos[2],
-        database:grupos[4]
-    });
-    
-    if(process.env.NODE_ENV == 'test') {
+    */
+    // mysql://b3a77af212021b:42310e7e@us-cdbr-iron-east-05.cleardb.net/heroku_87e9a1ece7b4c96?reconnect=true
+    if (process.env.NODE_ENV == 'production') {
+        var url = process.env.CLEARDB_DATABASE_URL;
+        var grupos = url.match(/mysql:\/\/(.*):(.*)@(.*)\/(.*)\?/);
+        return mysql.createConnection({
+            host: grupos[3],
+            user: grupos[1],
+            password: grupos[2],
+            database: grupos[4]
+        });
+    }
+
+    if (process.env.NODE_ENV == 'test') {
         return mysql.createConnection({
             host: 'localhost',
             user: 'root',
@@ -33,6 +34,6 @@ if (process.env.NODE_ENV == 'production') {
     }
 }
 
-module.exports = function() {
+module.exports = function () {
     return createDBConnection;
 }
